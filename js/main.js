6a6b802825ea8be15df161ebf1795c1bc8b21eb3
ankit kamboj <ankitkamboj777@gmail.com>
2020-08-12 13:35:58 +0530

    index.html

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
-window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementById("navbar").style.padding = "30px 10px";
-    document.getElementById("logo").style.fontSize = "25px";
-  } else {
-    document.getElementById("navbar").style.padding = "80px 10px";
-    document.getElementById("logo").style.fontSize = "35px";
-  }
-}
+//When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+
+// window.onscroll = function() {scrollFunction()};
+
+// function scrollFunction() {
+//   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+//     document.querySelector(".navbar").style.padding = "30px 10px";
+//     document.querySelector(".navbar-brand").style.fontSize = "25px";
+//   } else {
+//     document.querySelector(".navbar").style.padding = "50px 10px";
+//     document.querySelector(".navbar-brand").style.fontSize = "35px";
+//   }
+// }
