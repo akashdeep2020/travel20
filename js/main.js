1758a6733880bb1e82b34b761d5b20141e9e7ed8
ankit kamboj <ankitkamboj777@gmail.com>
2020-08-18 17:39:13 +0530

    youtube section

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).on('ready', function() {
+      
+    $(".regular").slick({
+      dots: true,
+      infinite: true,
+      slidesToShow: 3,
+      autoplay: true,
+      autoplaySpeed: 1000,
+      slidesToScroll: 1
+
+    });
+  });
